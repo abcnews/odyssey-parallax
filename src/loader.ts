601ee7c52e6cf1bdd063543e1fbc10be4a94328e
Colin Gourlay <colin@colin-gourlay.com>
2022-09-05 16:55:24 +1000

    Conditionally re-mount within (non-legacy) Odyssey Header

--- src/loader.ts
+++ src/loader.ts
@@ -1,3 +1,4 @@
+import type { Mount } from '@abcnews/mount-utils';
 import { selectMounts, getMountValue, isMount } from '@abcnews/mount-utils';
 import type { LayerData } from './components/layer';
 
@@ -7,10 +8,32 @@ type Section = {
   layers: LayerData[];
 };
 
+function conditionallyRemountWithinHeader(mountNode: Mount) {
+  // Check if we're inside Header-content
+  const headerContentEl: HTMLDivElement | null = mountNode.closest('.Header-content');
+
+  if (!headerContentEl || !headerContentEl.parentElement) {
+    return false;
+  }
+
+  // Remove Header-media if it exists
+  if (headerContentEl.previousElementSibling !== null) {
+    headerContentEl.parentElement.removeChild(headerContentEl.previousElementSibling);
+  }
+
+  headerContentEl.parentElement.insertBefore(mountNode, headerContentEl);
+
+  return true;
+}
+
 function getSection(mountNode: Element): Promise<Section> {
   if (!isMount(mountNode)) {
     throw new Error('Not a mount node');
   }
+
+  // Legacy Odyssey will have already re-mounted #parallax within .Header-media.
+  // This conditional re-mounting will apply in Presentation Layer-only Odyssey.
+  conditionallyRemountWithinHeader(mountNode);
 
   mountNode.classList.add('u-full');
 
